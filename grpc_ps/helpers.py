a28bf5eef5ec04ea779616585f084d632a1071fc
takumi19 <terrafin03@gmail.com>
2025-12-30 13:45:09 +0300

    fix tensor serialization issues

--- grpc_ps/helpers.py
+++ grpc_ps/helpers.py
@@ -4,11 +4,12 @@ from proto.ps_pb2 import TensorProto
 
 
 def serialize_tensor(tp: torch.Tensor) -> TensorProto:
-    array = tp.contiguous().numpy()
-    return TensorProto(data=array.tobytes(), dtype=str(array.dtype), shape=tp.shape)
+    with torch.no_grad():
+        array = tp.contiguous().numpy()
+        return TensorProto(data=array.tobytes(), dtype=str(array.dtype), shape=tp.shape)
 
 
 def deserialize_tensor(tp: TensorProto) -> torch.Tensor:
     array = np.frombuffer(tp.data, dtype=tp.dtype)
-    tensor = torch.from_numpy(array).reshape(tp.shape)
+    tensor = torch.from_numpy(array).reshape(tuple(tp.shape))
     return tensor
