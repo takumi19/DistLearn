21dd2e63d6c6e233ff08e689265b30569acc37b1
takumi19 <terrafin03@gmail.com>
2026-01-12 21:29:55 +0300

    simplest example

---
+++ decentralized/main.py
@@ -0,0 +1,20 @@
+import os
+import torch.distributed as dist
+
+def main():
+    dist.init_process_group(
+        backend="gloo",
+        init_method="env://",
+        rank=int(os.environ["RANK"]),
+        world_size=int(os.environ["WORLD_SIZE"]),
+    )
+
+    rank = dist.get_rank()
+    world_size = dist.get_world_size()
+
+    print(f"Hello from rank {rank} out of {world_size}!")
+
+    dist.destroy_process_group()
+
+if __name__ == "__main__":
+    main()
